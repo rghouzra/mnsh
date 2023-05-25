e7894e1dc6a98d38040aebb5d5c94a3dfaa54f54
Reda Ghouzraf <ghouzraf.reda@ofppt-edu.ma>
2023-05-25 20:34:08 +0100

    merging status -n 0.31

--- srcs/utils/readinput.c
+++ srcs/utils/readinput.c
@@ -3,14 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   readinput.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: rghouzra <rghouzra@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 06:57:44 by rghouzra          #+#    #+#             */
-/*   Updated: 2023/05/25 20:06:04 by yrhiba           ###   ########.fr       */
+/*   Updated: 2023/05/25 20:33:37 by rghouzra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mnsh.h"
+
+static void     print_dot(t_ast *node, int i, int *count, FILE *p)
+{
+        int     node_id;
+
+        if (node == 0x0)
+                return ;
+        node_id = ++(*count);
+        fprintf(p, "  node%d [label=\"%s\"];\n", node_id, node->value);
+        if (i != -666)
+                fprintf(p, "  node%d -> node%d;\n", i, node_id);
+        print_dot(node->left, node_id, count, p);
+        print_dot(node->right, node_id, count, p);
+}
+
+void    print_tree_dot(t_ast *root, char *s)
+{
+        int     count;
+        FILE *p;
+
+        p = fopen("treegraph.dot", "w");
+        fprintf(p, "/*\n%s\n*/\n", s);
+        fprintf(p, "digraph {\n");
+        count = 0;
+        print_dot(root, -666, &count, p);
+        fprintf(p, "}\n");
+        fflush(p);
+}
+
 
 void	read_input()
 {
@@ -32,6 +61,7 @@ void	read_input()
 		{
 			// add_history(s);
 			tree = shunting_algorithm(token);
+			print_tree_dot(tree, s);
 			// if (tree)
 				// eval_tree(tree, 0, (t_io){0, 1, -2, -2, 0});
 			tree_cleaner(&tree);
