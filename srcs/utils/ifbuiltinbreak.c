b7b61d9f7d72d24a90fdd28e76e7306276916b3b
yrhiba <yahyarhiba@gmail.com>
2023-05-29 17:52:59 +0100

    modified:   srcs/builtins/env/env.c 	modified:   srcs/utils/ifbuiltinbreak.c

--- srcs/utils/ifbuiltinbreak.c
+++ srcs/utils/ifbuiltinbreak.c
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 16:51:12 by yrhiba            #+#    #+#             */
-/*   Updated: 2023/05/29 17:12:19 by yrhiba           ###   ########.fr       */
+/*   Updated: 2023/05/29 17:40:33 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,6 @@ void	ifbuiltinbreak(char **cmnds)
 		return ;
 	if (my_string_compare("echo", *cmnds) == LS_EQUAL)
 		echo(my_strings_count(cmnds), cmnds);
+	else if (my_string_compare("env", *cmnds) == LS_EQUAL)
+		env();
 }
