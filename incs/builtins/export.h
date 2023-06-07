4729ad78fd8b70d76bb7c28537108ccce056bbff
yrhiba <yahyarhiba@gmail.com>
2023-06-07 17:55:13 +0100

    modified:   incs/builtins/export.h 	modified:   srcs/builtins/export/export.c

--- incs/builtins/export.h
+++ incs/builtins/export.h
@@ -6,12 +6,15 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 09:08:42 by yrhiba            #+#    #+#             */
-/*   Updated: 2023/06/07 17:46:38 by yrhiba           ###   ########.fr       */
+/*   Updated: 2023/06/07 17:48:53 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPORT_H
 # define EXPORT_H
+
+# define KEY_VALID 0
+# define KEY_NOTVALID 1
 
 void	export(int ac, char **av, int status);
 int		is_keyvalid(char *key);
