6c397dc43f664334f7ae7fd866df4f0b7458a425
Reda Ghouzraf <ghouzraf.reda@ofppt-edu.ma>
2023-06-21 00:02:35 +0100

    add: heredoc in expand

--- srcs/utils/read_heredoc.c
+++ srcs/utils/read_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: rghouzra <rghouzra@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 15:01:02 by rghouzra          #+#    #+#             */
-/*   Updated: 2023/06/20 20:49:26 by rghouzra         ###   ########.fr       */
+/*   Updated: 2023/06/21 00:00:27 by rghouzra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,78 @@ static void	heredoc_handler(int sig)
 	g_mnsh->_break = 1;
 }
 
-char	*read_heredoc(char *delimiter)
+typedef struct s_delim_expand
+{
+	int		i;
+	char 	**lines;
+	int		j;
+	int		dollar;
+	char 	c;
+	int		flag;
+}t_delim_expand;
+
+int	check_delim_expansion(char **line)
+{
+	t_delim_expand	packed;
+	
+	if (line == NULL || *line == NULL)
+		return (-1);
+	packed.i = -1;
+	packed.flag = 0;
+	packed.lines =  ft_alphasplit2(*line, '\0', (t_alphasplit){0, 0,0, 0,0, 0,0, 0,0});
+	while(packed.lines[++packed.i])
+	{
+		packed.j = -1;
+		packed.dollar = 0;
+		packed.c = packed.lines[packed.i][0];
+		while(packed.lines[packed.i][++packed.j])
+			packed.dollar += (packed.lines[packed.i][packed.j] == '$');
+		packed.flag += (!packed.dollar) + ((packed.dollar >= 1) \
+		 - ( +(packed.c == DQ) + (packed.c == SQ) - (!packed.dollar)));
+	}
+	ft_free(packed.lines);
+	remove_quotes(line);
+	printf("flag->%d\n", packed.flag);
+	return packed.flag >= packed.i;
+}
+
+typedef struct s_read_heredoc
 {
 	char	*line;
 	int		fd;
 	char	*name;
+	int		flag_expansion;
+	char	*ptr;
+}t_read_heredoc;
 
-	if (!delimiter)
+int readheredoc_init(t_read_heredoc *var, char *delimiter)
+{
+	var->flag_expansion = check_delim_expansion(&delimiter);
+	var->ptr = delimiter;
+	return var->flag_expansion;
+}
+char	*read_heredoc(char *delimiter)
+{
+	t_read_heredoc	x;
+
+	if (readheredoc_init(&x, delimiter) == -1)
 		return (NULL);
-	name = generate_filename();
-	fd = ft_open(name, O_CREAT | O_RDWR | O_TRUNC, 0644, &g_mnsh->exit_status);
+	x.name = generate_filename();
+	x.fd = ft_open(x.name, O_CREAT | O_RDWR | O_TRUNC, 0644, &g_mnsh->exit_status);
 	if (ft_fork() == 0)
 	{
 		signal(SIGINT, heredoc_handler);
 		while (g_mnsh->_break == 0)
 		{
-			line = readline("> ");
-			if (!line || !ft_strcmp(line, delimiter))
+			x.line = readline("> ");
+			if (!x.line || !ft_strcmp(x.line, delimiter))
 				break;
-			ft_putendl_fd(line, fd);
-			free(line);
-			
+			if(x.flag_expansion)
+				expand(&x.line);
+			ft_putendl_fd(x.line, x.fd);
+			free(x.line);
 		}
 		exit(0);
 	}
-	return (line = NULL, waitpid(-1, 0, 0), free(line), close(fd), name);
+	return (x.line = NULL, waitpid(-1, 0, 0), free(x.line), free(x.ptr), close(x.fd), x.name);
 }
