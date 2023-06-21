4a83dde81201c790e8fb763d14087b3f5425652d
Reda Ghouzraf <ghouzraf.reda@ofppt-edu.ma>
2023-06-21 04:18:47 +0100

    fix: memory leaks

--- srcs/tokenizer/analyze_token.c
+++ srcs/tokenizer/analyze_token.c
@@ -6,7 +6,7 @@
 /*   By: rghouzra <rghouzra@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 11:27:30 by rghouzra          #+#    #+#             */
-/*   Updated: 2023/06/20 23:46:51 by rghouzra         ###   ########.fr       */
+/*   Updated: 2023/06/21 04:06:18 by rghouzra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 void	analyze_token(t_list *token)
 {
+	char	*a;
+
 	if (token == NULL || token->next == NULL)
 		return ;
 	while (token->next)
 	{
 		if (token->type == heredoc_i && token->next->type == WORD)
+		{
+			a = token->next->content;
 			token->next->content = read_heredoc(token->next->content);
+			free(a);
+		}
 		token = token->next;
 	}
 }
