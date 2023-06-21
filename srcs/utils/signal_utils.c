fbb8f0f6a70e856e18dfecb9479d91493380da55
Reda Ghouzraf <ghouzraf.reda@ofppt-edu.ma>
2023-06-21 17:56:31 +0100

    fix the exit_status

--- srcs/utils/signal_utils.c
+++ srcs/utils/signal_utils.c
@@ -6,7 +6,7 @@
 /*   By: rghouzra <rghouzra@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:08:54 by rghouzra          #+#    #+#             */
-/*   Updated: 2023/06/21 10:17:18 by rghouzra         ###   ########.fr       */
+/*   Updated: 2023/06/21 17:38:00 by rghouzra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 
 static void	sigint_handler(int sig)
 {
-	g_mnsh->exit_status = 1;
 	(void)sig;
 	printf("\n");
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
+	g_mnsh->exit_status = -129;
 }
 
 static void	sigquit_handler(int sig)
