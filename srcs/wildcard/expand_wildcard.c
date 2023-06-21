f3d5cb5a9c6cb85ab740dc38465909c6afefb608
yrhiba <yahyarhiba@gmail.com>
2023-06-21 22:07:54 +0100

    fixing norm

--- srcs/wildcard/expand_wildcard.c
+++ srcs/wildcard/expand_wildcard.c
@@ -6,17 +6,19 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 07:26:00 by yrhiba            #+#    #+#             */
-/*   Updated: 2023/06/21 21:40:33 by yrhiba           ###   ########.fr       */
+/*   Updated: 2023/06/21 21:57:06 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mnsh.h"
 
-static	void	add_tonew_list(t_my_list *it, t_my_list **new_list)
+static void	add_tonew_list(t_my_list *it, t_my_list **new_list)
 {
 	while (it)
 	{
-		if (my_list_push_back(new_list, my_list_new_elem(my_string_dup((char *)it->data), free_string)) == -1)
+		if (my_list_push_back(new_list,
+				my_list_new_elem(my_string_dup((char *)it->data),
+					free_string)) == -1)
 			exit(EXIT_FAILURE);
 		it = it->next;
 	}
@@ -29,7 +31,8 @@ static void	wildcard_expand_word(t_my_list **new_list, char **word)
 	if (**word == '\'' || **word == '"')
 	{
 		remove_quotes(word);
-		if (my_list_push_back(new_list, my_list_new_elem(my_string_dup(*word), free_string)) == -1)
+		if (my_list_push_back(new_list, my_list_new_elem(my_string_dup(*word),
+					free_string)) == -1)
 			exit(EXIT_FAILURE);
 		return ;
 	}
@@ -39,11 +42,12 @@ static void	wildcard_expand_word(t_my_list **new_list, char **word)
 		add_tonew_list(list, new_list);
 		my_list_clear(&list, free_string);
 	}
-	else if (my_list_push_back(new_list, my_list_new_elem(my_string_dup(*word), free_string)) == -1)
+	else if (my_list_push_back(new_list, my_list_new_elem(my_string_dup(*word),
+				free_string)) == -1)
 		exit(EXIT_FAILURE);
 }
 
-static	void	wildcard_expand_node(t_my_list **new_list, char **s)
+static void	wildcard_expand_node(t_my_list **new_list, char **s)
 {
 	char	**words;
 	char	*tmp;
@@ -54,7 +58,8 @@ static	void	wildcard_expand_node(t_my_list **new_list, char **s)
 		tmp = *s;
 		remove_quotes_2(s);
 		free(tmp);
-		if (my_list_push_back(new_list, my_list_new_elem(my_string_dup(*s), free_string)) == -1)
+		if (my_list_push_back(new_list, my_list_new_elem(my_string_dup(*s),
+					free_string)) == -1)
 			exit(EXIT_FAILURE);
 		my_strings_free(&words);
 		return ;
@@ -66,7 +71,7 @@ static	void	wildcard_expand_node(t_my_list **new_list, char **s)
 void	expand_wildcard(t_ast *term)
 {
 	t_my_list	*new;
-	t_my_list 	*it;
+	t_my_list	*it;
 
 	my_list_init(&new);
 	it = term->value_expanded;
